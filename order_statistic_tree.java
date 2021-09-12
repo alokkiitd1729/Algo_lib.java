b14b0b8192de1b18925aeeaacfdde0d92eccc460
Alok Kumar <36418056+alokkiitd1729@users.noreply.github.com>
2021-09-12 17:45:41 +0530

    Update order_statistic_tree.java

--- order_statistic_tree.java
+++ order_statistic_tree.java
@@ -1,20 +1,21 @@
 // https://www.hackerrank.com/contests/goc-cdc-series-4/challenges/just-another-noob/problem
 
+ import java.util.*;
+ import java.io.*;
+ public class CSES{
+ // taking inputs
+ static BufferedReader s1;
+ static BufferedWriter out;
+ static String read() throws IOException{String line="";while(line.length()==0){line=s1.readLine();continue;}return line;}
+ static int int_v (String s1){return Integer.parseInt(s1);}
+ static long long_v(String s1){return Long.parseLong(s1);}
+ static int[] int_arr() throws IOException{String[] a=read().split(" ");int[] b=new int[a.length];for(int i=0;i<a.length;i++){b[i]=int_v(a[i]);}return b;}
+ static long[] long_arr() throws IOException{String[] a=read().split(" ");long[] b=new long[a.length];for(int i=0;i<a.length;i++){b[i]=long_v(a[i]);}return b;}
+ static void sort(int[] a){List<Integer> l=new ArrayList<>();for(int z:a){l.add(z);}Collections.sort(l);for(int i=0;i<a.length;i++){a[i]=l.get(i);}}
+ static void assign(){s1=new BufferedReader(new InputStreamReader(System.in));out=new BufferedWriter(new OutputStreamWriter(System.out));}
+ //......................................@uthor_Alx..............................................
 
-import java.util.*;
-import java.io.*;
-public class CSES{
-// taking inputs
-static BufferedReader s1;
-static BufferedWriter out;
-static String read() throws IOException{String line="";while(line.length()==0){line=s1.readLine();continue;}return line;}
-static int int_v (String s1){return Integer.parseInt(s1);}
-static long long_v(String s1){return Long.parseLong(s1);}
-static int[] int_arr() throws IOException{String[] a=read().split(" ");int[] b=new int[a.length];for(int i=0;i<a.length;i++){b[i]=int_v(a[i]);}return b;}
-static long[] long_arr() throws IOException{String[] a=read().split(" ");long[] b=new long[a.length];for(int i=0;i<a.length;i++){b[i]=long_v(a[i]);}return b;}
-static void sort(int[] a){List<Integer> l=new ArrayList<>();for(int z:a){l.add(z);}Collections.sort(l);for(int i=0;i<a.length;i++){a[i]=l.get(i);}}
-static void assign(){s1=new BufferedReader(new InputStreamReader(System.in));out=new BufferedWriter(new OutputStreamWriter(System.out));}
-//......................................@uthor_Alx..............................................
+// find kth largest/smallest element in a given multiset -  for high value do co-ordinate compressing     
 static int BIT[];
 static void update(int x, int delta){
        while(x<BIT.length){
@@ -45,20 +46,39 @@ static int find( int k,int n){.   // finds the least index j such that there are
     }
     return ans;
 }
- 
-public static void main(String[] args) throws  IOException{
-                        assign();
-                          int n=int_v(read());
-                          int[] arr=int_arr();
-                          BIT=new int[n+2];
-                          for(int i=1;i<=n;i++)update(i,1);
-                          int[] res=new int[n+1];
-                          for(int i=n-1;i>=0;i--){
-                              int tmp=find(arr[i],n);
-                              res[tmp]=i+1;
-                              update(tmp,-1);
-                          }
-                          for(int i=1;i<=n;i++)out.write(res[i]+" ");
-                          out.flush();
-    }
-}
+
+ public static void main(String[] args) throws  IOException{
+ 	        		    assign();
+ 	        		  	int[] n1=int_arr();
+ 	        		  	int n=n1[0],k=n1[1];
+ 	        		  	int[] arr=int_arr();
+ 	        		  	int[] c=arr.clone();
+ 	        		  	sort(c);
+ 	        		  	int j=1,k1=k;k=k/2+k%2;
+ 	        		  	Map<Integer,List<Integer>> map=new HashMap<>();
+ 	        		  	Map<Integer,Integer> ctr=new HashMap<>();
+ 	        		  	int[] map1=new int[n+2];
+ 	        		  	for(int x:c){
+ 	        		  		map1[j]=x;
+ 	        		  		ctr.put(x,0);
+ 	        		  		map.computeIfAbsent(x,key ->new ArrayList<>()).add(j);
+ 	        		  		j++;
+ 	        		  	}
+ 	        		  	int[] BIT=new int[n+2];
+ 	        		  	for(int i=0;i<n;i++){
+ 	        		  		int xx=ctr.get(arr[i]),mapped=map.get(arr[i]).get(xx);
+ 	        		  		ctr.put(arr[i],xx+1);
+ 	        		  		arr[i]=mapped;
+ 	        		  	}
+ 	        		  	for(int i=1;i<=k1;i++){
+ 	        		  		update(arr[i-1],1,BIT);
+ 	        		  	}
+ 	        		  	out.write(map1[find(BIT,k,n)]+" ");j=0;
+ 	        		  	for(int i=k1;i<n;i++){
+ 	        		  		update(arr[i],1,BIT);
+ 	        		  		update(arr[j],-1,BIT);
+ 	        		  		out.write(map1[find(BIT,k,n)]+" ");j++;
+ 	        		  	}
+                         out.flush();
+ 	}
+ }
