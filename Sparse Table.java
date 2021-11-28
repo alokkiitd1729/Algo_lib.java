5bee274c71cc60d0ad317e4394a83e1d8486e04f
Alok Kumar <36418056+alokkiitd1729@users.noreply.github.com>
2021-11-29 00:41:35 +0530

    Update Sparse Table.java

--- Sparse Table.java
+++ Sparse Table.java
@@ -1,1 +1,7 @@
-// do it mustt
+/*
+  do it mustt  - OR, MAX, MIN, GCD, AND
+  
+  if not Remember binary lifting is always there for computing all kind of function
+  dp[i][j]  = f(arr[i,i+1,...,i+(1<<j)-1])
+
+*/
